4b6abe8b16c3d7e3fc4e2a4ad94cedf6cb3f0f57
obayedsiam <obayedsiam@gmail.com>
2022-03-22 15:29:57 +0600

    connection created, statement executed and data received from database table

--- src/DriverClass.java
+++ src/DriverClass.java
@@ -1,13 +1,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DriverClass {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
-        try{
+        try {
             //Load the driver
-            Class.forName("com.mysql.jdbc.Driver");
+          //  Class.forName("com.mysql.jdbc.Driver");
 
             // Create Connection
             String url = "jdbc:mysql://localhost:3306/sonydb";
@@ -15,20 +17,29 @@ public class DriverClass {
             String password = "";
             Connection con = DriverManager.getConnection(url, username, password);
 
-            if(con.isClosed()){
+            if (con.isClosed()) {
                 System.out.println("Connection is closed");
-            }
-            else{
+            } else {
                 System.out.println("Connection created");
+
+                String query = "SELECT call_status FROM tv_details WHERE id = 20";
+
+                Statement st = con.createStatement();
+                ResultSet set = st.executeQuery(query);
+
+                while(set.next()){
+                    String callStatus = set.getString("call_status");
+                    System.out.println(callStatus);
+                }
+
+                con.close();
+
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String query = "SELECT call_status FROM tv_details WHERE id = 20";
 
-        //Statement st = con
 
     }
 }
